d54de1adfd289a066107c3725825300867444d30
ImmortalBeast0 <monishajay2408@gmail.com>
2026-08-19 13:45:55 +0530

    fix(backend): Auto-created PostgreSQL db, updated OpenApiConfig bean signature, and disabled standalone Eureka warnings

--- back-end/shortener-service/src/main/java/com/kce/shortener/config/OpenApiConfig.java
+++ back-end/shortener-service/src/main/java/com/kce/shortener/config/OpenApiConfig.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.models.info.Info;
 public class OpenApiConfig {
 
 	@Bean
-	public OpenAPI customOpenAPI() {
+	OpenAPI customOpenAPI() {
 		return new OpenAPI().info(new Info().title("URL Shortener Service API")
 				.description("API documentation for Double Rolling Hash URL Shortener, Redirects, and Analytics").version("1.0.0"));
 	}
